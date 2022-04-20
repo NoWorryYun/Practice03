223d3d343e50c2d61ba949471a011d4be872bb3d
윤무걱정 <dbstjd3201@naver.com>
2022-04-20 10:28:50 +0900

    2022-04-20 랜덤숫자맞추기게임 random수정

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -7,7 +7,7 @@ public class Ex20 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int dap = 55;
+		int dap = (int)(Math.random()*100)+1;
 
 		System.out.println("================================");
 		System.out.println("\t[숫자맞추기게임 시작]");
@@ -28,8 +28,9 @@ public class Ex20 {
 					break;
 				} else if (yes_or_no.equals("n")) {
 					System.out.println("게임이 다시 시작됩니다.");
+					dap = (int)(Math.random()*100)+1;
 				}
-				
+
 			} else if (num > dap) {
 				System.out.println("더 낮게");
 			} else if (num < dap) {
